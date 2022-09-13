3353b811c68373983ab68cae86dafa8742d665c4
István Derda <mail@istvan-derda.de>
2022-09-13 11:16:52 +0200

    add beautiful commandline handling :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,37 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-public class Main {
+import java.io.IOException;
+
+@Command
+public class Main implements Runnable {
+
+    @Parameters(index = "0", description = "path of source file to parse")
+    String whileProgramSourceName;
 
     public static void main(String[] args) {
-        CharStream inputStream;
-        if (args.length == 0) {
-            inputStream = CharStreams.fromString("x = 3;" + "y=7;" + "z=y+9;" + "xcpy = x;");
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(whileProgramSourceName);
+
+        try {
+            CharStream inputStream = CharStreams.fromFileName(whileProgramSourceName);
+            WhileLexer lexer = new WhileLexer(inputStream);
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            WhileParser parser = new WhileParser(tokenStream);
+            WhileParser.ProgramsContext programContext = parser.programs();
+            PrintToConsoleListener printToConsoleListener = new PrintToConsoleListener();
+            ParseTreeWalker.DEFAULT.walk(printToConsoleListener, programContext);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        else {
-            inputStream = null; //todo read file
-        }
-        WhileLexer lexer = new WhileLexer(inputStream);
-        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        WhileParser parser = new WhileParser(tokenStream);
-        WhileParser.ProgramsContext programContext = parser.programs();
-        PrintToConsoleListener printToConsoleListener = new PrintToConsoleListener();
-        ParseTreeWalker.DEFAULT.walk(printToConsoleListener, programContext);
     }
 }
